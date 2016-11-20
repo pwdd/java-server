899837d4f42841e0a95cf831355d6a0f56b2593e
pwdd <pwdd@users.noreply.github.com>
2016-11-20 13:23:55 -0600

    Add parser to get directory name

--- src/main/java/com/pwdd/httpServer/HTTPServer.java
+++ src/main/java/com/pwdd/httpServer/HTTPServer.java
@@ -2,6 +2,8 @@ package com.pwdd.httpServer;
 
 public class HTTPServer {
   public static void main( String[] args ) {
+    Router.exitInvalidDir(args);
+    String dir = Router.getDirectory(args);
     new Server().run();
   }
 }
--- src/main/java/com/pwdd/httpServer/Router.java
+++ src/main/java/com/pwdd/httpServer/Router.java
@@ -10,4 +10,26 @@ final class Router {
       return "";
     }
   }
+
+  private static Boolean isValidDirectory(String dirName) {
+    return (dirName.equals("foo") || dirName.equals("bar"));
+  }
+
+  static String getDirectory(String[] args) {
+    if (args.length == 0) {
+      return "foo";
+    }
+    else if (isValidDirectory(args[0])) {
+      return args[0];
+    } else {
+      return "foo";
+    }
+  }
+
+  static void exitInvalidDir(String[] args) {
+    if (args.length > 0 && !isValidDirectory(args[0])) {
+      System.out.println("not a valid directory name");
+      System.exit(0);
+    }
+  }
 }
