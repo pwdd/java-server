b613a65d40e8e1bea49d703ab99a250f356bc77d
pwdd <pwdd@users.noreply.github.com>
2016-11-23 13:25:12 -0600

    Reorganize ArgumentsValidation

--- src/main/java/com/pwdd/httpServer/ArgumentsValidation.java
+++ src/main/java/com/pwdd/httpServer/ArgumentsValidation.java
@@ -9,29 +9,15 @@ final class ArgumentsValidation {
 
   private ArgumentsValidation() {}
 
-  static Boolean isValidArgs(String[] args) {
-    return args.length == 0 || ((startsWithFlag(args) && !sameArg(args)) && hasPortOrDir(args));
-  }
-
-  static String getDirectory(String[] args) {
-    return getArg(args, "-d", defaultDir);
-  }
-
-  static String getPortNumber(String[] args) {
-    return getArg(args, "-p", defaultPortNumber);
-  }
-
   static void exitOnInvalidArgs(String[] args) {
     if (!isValidArgs(args) || !isValidPortNumber(getPortNumber(args))) {
-        System.out.println("invalid arguments");
-        System.exit(0);
+      System.out.println("invalid arguments");
+      System.exit(0);
     }
   }
 
-  private static Boolean hasPortOrDir(String[] args) {
-    return (args.length == 2 || args.length == 4) &&
-        getIndexOf(args, "-d") != -1 &&
-        getIndexOf(args, "-p") != -1;
+  static Boolean isValidArgs(String[] args) {
+    return args.length == 0 || ((startsWithFlag(args) && !sameArg(args)) && hasPortOrDir(args));
   }
 
   private static Boolean startsWithFlag(String[] args) {
@@ -49,6 +35,30 @@ final class ArgumentsValidation {
     return args.length == 4 && args[0].equals(args[2]);
   }
 
+  private static Boolean hasPortOrDir(String[] args) {
+    return (args.length == 2 || args.length == 4) &&
+        getIndexOf(args, "-d") != -1 &&
+        getIndexOf(args, "-p") != -1;
+  }
+
+  static String getDirectory(String[] args) {
+    return getArg(args, "-d", defaultDir);
+  }
+
+  static String getPortNumber(String[] args) {
+    return getArg(args, "-p", defaultPortNumber);
+  }
+
+  private static String getArg(String[] args, String arg, String defaultResult) {
+    List<String> argsList = Arrays.asList(args);
+    if (argsList.contains(arg)) {
+      return args[getIndexOf(args, arg)];
+    }
+    else {
+      return defaultResult;
+    }
+  }
+
   private static Boolean isValidPortNumber(String port) {
     if (isNumeric(port)) {
       int portNumber = Integer.parseInt(port);
@@ -60,16 +70,6 @@ final class ArgumentsValidation {
 
   private static Boolean isNumeric(String str) {
     return str.matches("\\d+");
-  }
-
-  private static String getArg(String[] args, String arg, String defaultResult) {
-    List<String> argsList = Arrays.asList(args);
-    if (argsList.contains(arg)) {
-      return args[getIndexOf(args, arg)];
-    }
-    else {
-      return defaultResult;
-    }
   }
 
   private static int getIndexOf(String[] args, String arg) {
